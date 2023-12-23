3b1c338bac9055d1bbe161cda82b48b3ee2199c7
Sameet Keshav <sameet.keshav@gmail.com>
2023-12-23 11:11:30 +0200

    Completed the My Projects Section

---
+++ src/app/components/ProjectsSection.jsx
@@ -0,0 +1,68 @@
+"use client"
+import React, { useState } from "react";
+import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
+
+const projectsData =[
+  {
+    id: 1,
+    title: "Encompass",
+    description: "Description",
+    image: "/images/projects/encompass.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/COS301-SE-2023/Encompass.git",
+    previewUrl: "https://encompass-hosting.onrender.com"
+  }
+]
+
+const ProjectsSection = ({ imgUrl, title, description }) => {
+  const [tag, setTag] = useState("All");
+
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  }
+
+  const filteredProjects = projectsData.filter((project) =>
+    project.tag.includes(tag)
+  );
+
+  return (
+    <div>
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+        My Projects
+      </h2>
+      <div className="text-white flex flex-row justify-center items-center gap-2 my-6">
+        <ProjectTag 
+          onClick={handleTagChange} 
+          name="All" 
+          isSelected={tag === "All"} 
+        />
+         <ProjectTag 
+          onClick={handleTagChange} 
+          name="Web" 
+          isSelected={tag === "Web"} 
+        />
+
+        <ProjectTag 
+          onClick={handleTagChange} 
+          name="Mobile" 
+          isSelected={tag === "Mobile"} 
+        />
+      </div>
+      <div className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {filteredProjects.map((project) => (
+          <ProjectCard 
+            key={project.id}
+            title={project.title}
+            description={project.description}
+            imgUrl={project.image}
+            gitUrl={project.gitUrl}
+            previewUrl={project.previewUrl}
+          /> 
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default ProjectsSection
