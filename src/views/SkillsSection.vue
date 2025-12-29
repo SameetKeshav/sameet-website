1e35f0ef25f3de7ef4a97c2a35d64ac0353e5214
Sameet Keshav <sameet.keshav@gmail.com>
2025-12-29 17:08:22 +0200

    feat: Add all skills to skill section

--- src/views/SkillsSection.vue
+++ src/views/SkillsSection.vue
@@ -2,29 +2,38 @@
   <section id="skills" class="max-w-4xl mx-auto">
     <h2 class="text-3xl font-semibold text-center mb-8">Technical Skills</h2>
 
-    <div v-for="skill in skills" :key="skill.name" class="mb-6">
-      <div class="flex justify-between items-baseline mb-1">
-        <span class="font-medium">{{ skill.name }}</span>
-        <!-- <span class="text-sm text-gray-500 dark:text-gray-400">{{ skill.level }}%</span> -->
-      </div>
+   <ul class="pl-6">
+    <li v-for="skill in skills" :key="skill" class="flex flex-col relative pl-5">
+      <!-- Custom bullet -->
+      <span class="absolute left-0 top-1/2 transform -translate-y-1/2 w-2 h-2 bg-indigo-500 rounded-full"></span>
 
-      <!-- Progress bar
-      <div class="w-full bg-gray-200 rounded h-2.5 dark:bg-gray-700">
-        <div
-          :style="{ width: `${skill.level}%` }"
-          class="h-2.5 rounded bg-indigo-600 transition-all duration-300 ease-in-out"
-        ></div>
-      </div> -->
-    </div>
+      <!-- Skill name -->
+      <div class="flex justify-between items-baseline mb-1">
+        <span class="font-medium">{{ skill }}</span>
+      </div>
+    </li>
+  </ul>
+
+
+
   </section>
 </template>
 
 <script lang="ts" setup>
-const skills = [
-  { name: 'Vue.js' },
-  { name: 'TypeScript' },
-  { name: 'Tailwind CSS' },
-  { name: 'JavaScript' },
-  { name: 'HTML/CSS' },
-]
+  const skills = [
+    'AWS',
+    'Vue.js',
+    'Typescript',
+    'Tailwind CSS',
+    'JavaScript',
+    'HTML/CSS',
+    'React',
+    'Next.js',
+    'NestJS',
+    'MySQL',
+    'MongoDB',
+    'Java',
+    'Python',
+    'C++'
+  ]
 </script>
